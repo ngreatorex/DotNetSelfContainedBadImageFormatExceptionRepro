7354757a5630bf36a7950a2153ca3e78d77e5037
Neil Greatorex <neil@greatorex.me.uk>
2025-02-20 11:30:23 +0000

    Add project files.

---
+++ SelfContainedExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.FlightSimulator.SimConnect;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+var simEvent = new AutoResetEvent(false);
+
+SimConnect simConnect;
+try
+{
+    simConnect = new SimConnect("Self Contained Test", Process.GetCurrentProcess().Handle, 0, simEvent, 0);
+}
+catch (COMException ex)
+{
+    Console.WriteLine("Failed to connect to Flight Simulator: " + ex.Message);
+    return;
+}
+
+simConnect.OnRecvOpen += (sender, data) => Console.WriteLine("Connected to Flight Simulator");
+
+Console.WriteLine("Waiting for sim event...");
+simEvent.WaitOne();
